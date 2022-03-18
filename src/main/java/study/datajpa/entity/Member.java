e9d56d37d6c568607a626a3ad7878d5294cee01d
simple <shwncho4@gmail.com>
2022-03-18 21:36:45 +0900

    Test: Query Method by Spring Data JPA

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -20,6 +20,10 @@ public class Member {
     @JoinColumn(name = "team_id")
     private Team team;
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 
     public Member(String username) {
         this.username = username;
