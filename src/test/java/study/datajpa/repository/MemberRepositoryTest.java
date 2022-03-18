2ff6fee72173551336bc7789ebf0b2fddf5dfee3
simple <shwncho4@gmail.com>
2022-03-18 18:37:11 +0900

    Feat: Create Domain

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,8 +7,6 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.*;
 
 
